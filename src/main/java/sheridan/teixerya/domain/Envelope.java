dab7031a1627dfae2128e79edfa843467a651455
Ryan Teixeira <ryan.m.tex@gmail.com>
2021-05-27 23:39:09 -0400

    Added text input and radio buttons with error handling

--- src/main/java/sheridan/teixerya/domain/Envelope.java
+++ src/main/java/sheridan/teixerya/domain/Envelope.java
@@ -3,6 +3,7 @@ package sheridan.teixerya.domain;
 import java.io.Serializable;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 public class Envelope implements Serializable {
 
@@ -10,8 +11,20 @@ public class Envelope implements Serializable {
     @Max(1)
     private int message;
 
+    @NotBlank
+    private String firstName = "";
+
+    @NotBlank
+    private String lastName = "";
+
 
     public Envelope() {
+    }
+
+    public Envelope(int message,String firstName,String lastName) {
+        this.message = message;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public int getMessage() {
@@ -22,12 +35,28 @@ public class Envelope implements Serializable {
         this.message = message;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
     @Override
     public String toString() {
         return "Envelope{" +
                 "message=" + message +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
                 '}';
     }
 }
