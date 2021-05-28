0b13d2b864676df2be92176f7bb938363c00ab8f
Ryan Teixeira <ryan.m.tex@gmail.com>
2021-05-27 21:44:04 -0400

    Created Input & Output html with controller and dependencies

---
+++ src/main/java/sheridan/teixerya/domain/Envelope.java
@@ -0,0 +1,41 @@
+package sheridan.teixerya.domain;
+
+import java.io.Serializable;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+public class Envelope implements Serializable {
+
+    @Min(0)
+    @Max(2)
+    private int message = 1;
+    // no need to validate the boolean
+    private boolean registered = false;
+
+    public Envelope() {
+    }
+
+    public int getMessage() {
+        return message;
+    }
+
+    public void setMessage(int message) {
+        this.message = message;
+    }
+
+    public boolean isRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(boolean registered) {
+        this.registered = registered;
+    }
+
+    @Override
+    public String toString() {
+        return "Envelope{" +
+                "message=" + message +
+                ", registered=" + registered +
+                '}';
+    }
+}
